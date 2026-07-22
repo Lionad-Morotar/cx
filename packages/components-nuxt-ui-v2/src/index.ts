183ccab6d8acff6af0bc5fcf34335dd2579550b0
仿生狮子 <1806234223@qq.com>
2026-07-22 21:01:45 +0800

    feat(components-nuxt-ui-v2): v2 物料 bundle 导出
    - CxNuxtUIBundle：vendored Nuxt UI v2 物料自描述单元（含 simple-card），供装配方按 bundle 装配

--- packages/components-nuxt-ui-v2/src/index.ts
+++ packages/components-nuxt-ui-v2/src/index.ts
@@ -1,2 +1,12 @@
+import CxNuxtUI from './nuxt-ui-2'
+import CxSimpleCard from './simple-card'
+import type { CxMaterialBundle } from '@lionad/cx-definition'
+
 export { default as CxNuxtUI } from './nuxt-ui-2'
 export { default as CxSimpleCard } from './simple-card'
+
+/** nuxt-ui 物料 bundle：vendored Nuxt UI v2 物料自描述单元，供装配方（cx-nuxt 等）按 bundle 装配 */
+export const CxNuxtUIBundle: CxMaterialBundle = {
+  name: 'nuxt-ui',
+  materials: [...CxNuxtUI, CxSimpleCard],
+}
