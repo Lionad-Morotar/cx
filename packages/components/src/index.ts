3d106088e3da310e7be8d55e04f11d733c9a037b
仿生狮子 <1806234223@qq.com>
2026-07-22 21:01:45 +0800

    feat(components): components 物料 bundle 导出
    - CxComponentsBundle：基础物料自描述单元（page/grid/calendar/user-style/basics），供装配方按 bundle 装配

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,18 @@
+import CxBasics from './basic'
+import CxGrid from './grid'
+import CxCalendar from './calendar'
+import CxPage from './page'
+import CxUserStyle from './user-style'
+import type { CxMaterialBundle } from '@lionad/cx-definition'
+
 export { default as CxBasics } from './basic'
 export { default as CxGrid } from './grid'
 export { default as CxCalendar } from './calendar'
 export { default as CxPage } from './page'
 export { default as CxUserStyle } from './user-style'
+
+/** components 物料 bundle：基础物料自描述单元，供装配方（cx-nuxt 等）按 bundle 装配 */
+export const CxComponentsBundle: CxMaterialBundle = {
+  name: 'components',
+  materials: [CxPage, CxGrid, CxCalendar, CxUserStyle, ...CxBasics],
+}
